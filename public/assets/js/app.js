1efa77d9a230a3c415905da66c00f3ab8616ebfd
Aymane Chaaba <aymanechaaba@gmail.com>
2021-12-19 22:05:54 +0000

    First Commit

---
+++ public/assets/js/app.js
@@ -0,0 +1,6 @@
+var capital = document.getElementById("capital");
+var margin = document.getElementById("margin");
+
+var customForm = document.querySelector("custom-form");
+
+customForm.addEventListener("submit").
